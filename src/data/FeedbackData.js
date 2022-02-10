3cc8a42a34506f739a1e94b885c86448cbf71cde
Kritika <kritikadutt1997@gmail.com>
2022-02-10 19:02:18 +0530

    app created

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text:'This is an example of a feedback item1'
+    },
+    {
+        id:2,
+        rating:9,
+        text:'This is an example of a feedback item2'
+    },
+    {
+        id:3,
+        rating:8,
+        text:'This is an example of a feedback item3'
+    }
+]
+
+export default FeedbackData
